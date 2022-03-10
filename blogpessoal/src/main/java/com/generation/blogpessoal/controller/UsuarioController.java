2e3f863960ca878ceb925002100d5605a18d7c20
Carolina Lopes <carolina.b.lopes@hotmail.com>
2022-03-10 19:31:07 -0300

    Deploy do Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -5,16 +5,10 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-import com.generation.blogpessoal.model.Usuario;
-import com.generation.blogpessoal.model.UsuarioLogin;
-import com.generation.blogpessoal.repository.UsuarioRepository;
-import com.generation.blogpessoal.service.UsuarioService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +16,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -100,15 +99,15 @@ public class UsuarioController {
 	}
 	
 	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
-		
-		return usuarioRepository.findById(id)
-				.map(resposta -> {
-					usuarioRepository.deleteById(id);
-					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-				})
-				.orElse(ResponseEntity.notFound().build());
-	}
+//	@DeleteMapping("/{id}")
+//	public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
+//		
+//		return usuarioRepository.findById(id)
+//				.map(resposta -> {
+//					usuarioRepository.deleteById(id);
+//					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+//				})
+//				.orElse(ResponseEntity.notFound().build());
+//	}
 
 }
